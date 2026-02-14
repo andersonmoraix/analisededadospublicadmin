c3a63a23aca73bff11a74f5a712010f443b0f010
Anderson Morais <anderson.mouraissx@gmail.com>
2026-02-14 20:53:58 +0000

    Transformacao Logaritimica

---
+++ Exercício.R
@@ -0,0 +1,20 @@
+#Exercício
+#Faça o box-plot de população considerando a distribuição por região do Brasil.
+
+
+library(tidyverse)
+
+arquivo <- "https://raw.githubusercontent.com/fernandobarbalho/enap_auto_instucional/main/data/dados_municipios.csv"
+
+dados_municipios <- read_csv(file = arquivo)
+
+# Boxplot da população por região
+dados_municipios %>%
+  ggplot(aes(x = nome_regiao, y = populacao)) +
+  geom_boxplot() +
+  scale_y_log10() +
+  labs(
+    title = "Distribuição da População dos Municípios por Região",
+    x = "Região do Brasil",
+    y = "População (escala log10)"
+  )
